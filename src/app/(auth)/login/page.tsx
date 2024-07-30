794f49f251bad780c5a82cda2a01366f745ddcfe
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2024-07-30 22:44:32 +0100

    setup vendor dashboard

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,11 +2,10 @@ import type { Metadata } from "next";
 import Login from "@sections/auth/Login";
 
 export const metadata: Metadata = {
-  title: "Login - The Best React eCommerce Template",
-  description:
-    "Bonik is a React Next.js E-commerce template. Build SEO friendly Online store, delivery app and Multi vendor store",
-  authors: [{ name: "UI-LIB", url: "https://ui-lib.com" }],
-  keywords: ["e-commerce", "e-commerce template", "next.js", "react", "bonik"]
+  title: "Login - Vendorspot",
+  description: "Vendorspot is the No 1 Ecommerce PLatform",
+  authors: [{ name: "Stephen Chris", url: "https://stephenchris.com" }],
+  keywords: ["e-commerce", "e-commerce ", "next.js", "react", "vendorspot"],
 };
 
 export default function LoginPage() {
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,11 +2,10 @@ import type { Metadata } from "next";
 import Signup from "@sections/auth/Signup";
 
 export const metadata: Metadata = {
-  title: "Register - The Best React eCommerce Template",
-  description:
-    "Bonik is a React Next.js E-commerce template. Build SEO friendly Online store, delivery app and Multi vendor store",
-  authors: [{ name: "UI-LIB", url: "https://ui-lib.com" }],
-  keywords: ["e-commerce", "e-commerce template", "next.js", "react", "bonik"]
+  title: "Login - Vendorspot",
+  description: "Vendorspot is the No 1 Ecommerce PLatform",
+  authors: [{ name: "Stephen Chris", url: "https://stephenchris.com" }],
+  keywords: ["e-commerce", "e-commerce ", "next.js", "react", "vendorspot"],
 };
 
 export default function SignUpPage() {
