37dddccf26b6aef1fdcd385a7b1fe1d3a6419ccb
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2024-09-07 22:26:22 +0100

    updated mobile nav for vendors

--- src/components/layout/DashboardNavigation.tsx
+++ src/components/layout/DashboardNavigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import { usePathname } from "next/navigation";
 import { useAuth } from "@context/authContext";
 
@@ -12,7 +12,6 @@ import { DashboardNavigationWrapper, StyledDashboardNav } from "./styles";
 export default function DashboardNavigation() {
   const pathname = usePathname();
   const { user } = useAuth();
-  const [expandedItems, setExpandedItems] = useState({});
 
   const customerLinks = [
     {
@@ -52,13 +51,11 @@ export default function DashboardNavigation() {
           title: "Products",
           iconName: "box",
           count: null,
-          subItems: [
-            {
-              href: "/vendor/products/create",
-              title: "Create Product",
-              iconName: "plus-circle",
-            },
-          ],
+        },
+        {
+          href: "/vendor/products/create",
+          title: "Create Product",
+          iconName: "plus-circle",
         },
         {
           href: "/vendor/orders",
@@ -66,23 +63,11 @@ export default function DashboardNavigation() {
           iconName: "bag",
           count: null,
         },
-        // {
-        //   href: "/vendor/customers",
-        //   title: "Customers",
-        //   iconName: "user-group",
-        //   count: null,
-        // },
       ],
     },
     {
       title: "ACCOUNT SETTINGS",
       list: [
-        // {
-        //   href: "/vendor/profile",
-        //   title: "Profile Info",
-        //   iconName: "user",
-        //   count: null,
-        // },
         {
           href: "/vendor/account-settings",
           title: "Settings",
@@ -94,10 +79,6 @@ export default function DashboardNavigation() {
   ];
 
   const linkList = user?.role === "Vendor" ? vendorLinks : customerLinks;
-
-  const toggleExpand = (title) => {
-    setExpandedItems((prev) => ({ ...prev, [title]: !prev[title] }));
-  };
 
   return (
     <DashboardNavigationWrapper
@@ -113,72 +94,29 @@ export default function DashboardNavigation() {
           </Typography>
 
           {item.list.map((listItem) => (
-            <Fragment key={listItem.title}>
-              <StyledDashboardNav
-                px="1.5rem"
-                mb="1.25rem"
-                href={listItem.href}
-                isCurrentPath={pathname.includes(listItem.href)}
+            <StyledDashboardNav
+              key={listItem.title}
+              px="1.5rem"
+              mb="1.25rem"
+              href={listItem.href}
+              isCurrentPath={pathname.includes(listItem.href)}
+            >
+              <FlexBox
+                alignItems="center"
+                justifyContent="space-between"
+                width="100%"
               >
-                <FlexBox
-                  alignItems="center"
-                  justifyContent="space-between"
-                  width="100%"
-                >
-                  <FlexBox alignItems="center">
-                    <div className="dashboard-nav-icon-holder">
-                      <Icon
-                        variant="small"
-                        defaultcolor="currentColor"
-                        mr="10px"
-                      >
-                        {listItem.iconName}
-                      </Icon>
-                    </div>
-                    <span>{listItem.title}</span>
-                  </FlexBox>
-                  {listItem.subItems && (
-                    <Icon
-                      variant="small"
-                      defaultcolor="currentColor"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        toggleExpand(listItem.title);
-                      }}
-                    >
-                      {expandedItems[listItem.title] ? "minus" : "plus"}
+                <FlexBox alignItems="center">
+                  <div className="dashboard-nav-icon-holder">
+                    <Icon variant="small" defaultcolor="currentColor" mr="10px">
+                      {listItem.iconName}
                     </Icon>
-                  )}
+                  </div>
+                  <span>{listItem.title}</span>
                 </FlexBox>
-                {listItem.count !== null && <span>{listItem.count}</span>}
-              </StyledDashboardNav>
-              {listItem.subItems && expandedItems[listItem.title] && (
-                <Fragment>
-                  {listItem.subItems.map((subItem) => (
-                    <StyledDashboardNav
-                      key={subItem.title}
-                      px="3rem"
-                      mb="1.25rem"
-                      href={subItem.href}
-                      isCurrentPath={pathname.includes(subItem.href)}
-                    >
-                      <FlexBox alignItems="center">
-                        <div className="dashboard-nav-icon-holder">
-                          <Icon
-                            variant="small"
-                            defaultcolor="currentColor"
-                            mr="10px"
-                          >
-                            {subItem.iconName}
-                          </Icon>
-                        </div>
-                        <span>{subItem.title}</span>
-                      </FlexBox>
-                    </StyledDashboardNav>
-                  ))}
-                </Fragment>
-              )}
-            </Fragment>
+              </FlexBox>
+              {listItem.count !== null && <span>{listItem.count}</span>}
+            </StyledDashboardNav>
           ))}
         </Fragment>
       ))}
