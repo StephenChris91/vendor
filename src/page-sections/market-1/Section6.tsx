028f7f021b68a694019f849b02ef079eca67a663
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2024-09-21 23:43:44 +0100

    updated home page to fit specifications

--- src/page-sections/market-1/Section6.tsx
+++ src/page-sections/market-1/Section6.tsx
@@ -1,6 +1,6 @@
 // Section6.tsx
 
-import ClientSection6 from "@component/section6/clientSection6";
+import ClientSection6 from "../../components/section6/clientSection6";
 
 export default function Section6() {
   return <ClientSection6 />;
